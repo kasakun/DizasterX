a26d2d37804181a049a4c23f21469e2a76656db9
Zeyu Chen <chenzy@gatech.edu>
2018-06-03 18:41:23 -0700

    api built

---
+++ API/run.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+# Script for compile and run.
+
+mvn clean compile
+
+mvn exec:java
+
+
+
--- API/src/main/java/com/DizasterX/MyResource.java
+++ API/src/main/java/com/DizasterX/MyResource.java
@@ -5,10 +5,29 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("hello")
 public class MyResource {
 
     /**
@@ -20,6 +39,10 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("DizasterX");
+        MongoCollection<Document> collection = database.getCollection("data");
+        Document doc = collection.find(eq("title", "TORNADO")).first();
+        return doc.toJson();
     }
 }
