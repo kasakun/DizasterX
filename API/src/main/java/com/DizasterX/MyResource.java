697753c75b4cb2f2c67975476849b2c1aaa8bfea
Zeyu Chen <chenzy@gatech.edu>
2018-06-13 00:46:52 -0700

    delete redundant packages.

--- API/src/main/java/com/DizasterX/MyResource.java
+++ API/src/main/java/com/DizasterX/MyResource.java
@@ -7,21 +7,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.QueryParam;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.ServerAddress;
-
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
-import java.util.Arrays;
 import com.mongodb.Block;
 
-import com.mongodb.client.MongoCursor;
 import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.result.DeleteResult;
-import static com.mongodb.client.model.Updates.*;
-import com.mongodb.client.result.UpdateResult;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
